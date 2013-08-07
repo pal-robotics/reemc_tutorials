21839bfab6bf6ce19e31499f43ded43846d5ad5b
Victor Lopez <victor.lopez@pal-robotics.com>
2013-08-07 11:22:08 +0000

    Finished look_at_detected_face.py SMACH example refs #6336
    git-svn-id: svn+ssh://server/srv/svn/repos/branches/OROCOS_2.X/pal-ros-pkg/stacks/reemc_tutorials@44209 5e370ff8-3418-0410-babe-3378cc20a00d

--- scripts/look_at_detected_face.py
+++ scripts/look_at_detected_face.py
@@ -3,23 +3,30 @@
 import roslib; roslib.load_manifest('reemc_tutorials')
 import rospy
 import smach
+import math
 import smach_ros
-from smach_ros import MonitorState, SimpleActionState, IntrospectionServer, ActionServerWrapper
-import pal_detection_msgs
-import sensor_msgs.msg
+import actionlib
+import pal_detection_msgs.msg
+#from pal_detection_msgs.msg import FaceDetections
+import sensor_msgs.msg 
 import geometry_msgs.msg 
+import pr2_controllers_msgs.msg
+
 
 
 # define state Foo
 class GetFaceDetectionState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success'])
-        self.sub = rospy.Subscriber(name="/pal_face/faces", data_class=pal_detection_msgs.FaceDetections, callback=self.face_callback)
+        smach.State.__init__(self, outcomes=['success'],
+                                output_keys=['last_face_detected'])
+        self.sub = rospy.Subscriber(name="/pal_face/faces", 
+                                    data_class=pal_detection_msgs.msg.FaceDetections, 
+                                    callback=self.face_callback)
         self.last_face_detected = None
     def face_callback(self, msg):
-        rospy.loginfo("Received %d detected faces", len(faces))
-        if len(faces) > 0:
-            self.last_face_detected = msg[0]
+        #rospy.loginfo("Received %d detected faces", len(msg.faces))
+        if len(msg.faces) > 0:
+            self.last_face_detected = msg.faces[0]
         
     def execute(self, userdata):
         self.last_face_detected = None
@@ -27,15 +34,22 @@ class GetFaceDetectionState(smach.State):
         while self.last_face_detected == None:
             rospy.sleep(0.1)
 
-        rospy.loginfo('Face detected')
         userdata.last_face_detected = self.last_face_detected
+
+        return 'success'
+        
         
         
 class LookToPointState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success', 'failed'])
+        smach.State.__init__(self, outcomes=['success', 'failed'],
+                                input_keys=['last_face_detected'])
         self.camera_info = None
-        self.sub = rospy.Subscriber(name="/stereo/left/camera_info", data_class=sensor_msgs.CameraInfo, callback=self.camera_info_callback)
+        self.sub = rospy.Subscriber(name="/stereo/left/camera_info", 
+                                    data_class=sensor_msgs.msg.CameraInfo, 
+                                    callback=self.camera_info_callback)
+        self.client = actionlib.SimpleActionClient('/head_traj_controller/point_head_action',
+                                                   pr2_controllers_msgs.msg.PointHeadAction)
 
 
     #Obtain the camera instrinsic parameters        
@@ -49,8 +63,8 @@ class LookToPointState(smach.State):
             rospy.logerr("Camera intrinsic parameters not received")
             return 'failed'
         
-        targetX = userdata.last_face_detected.x + width/2
-        targetY = userdata.last_face_detected.y + height/2
+        targetX = userdata.last_face_detected.x + userdata.last_face_detected.width/2
+        targetY = userdata.last_face_detected.y + userdata.last_face_detected.height/2
             
     
         #K[0] is fx
@@ -60,95 +74,66 @@ class LookToPointState(smach.State):
     
         x = (targetX - self.camera_info.K[2])/self.camera_info.K[0]
         y = (targetY - self.camera_info.K[5])/self.camera_info.K[4]
-        z = 1 #Arbitrary number
+        z = 1 #Arbitrary distance
+        
+        if math.sqrt(pow(x, 2) + pow(y, 2)) < 0.2:
+            return 'success'
+        pointStamped = geometry_msgs.msg.PointStamped()      
+        pointStamped.header.frame_id = "/stereo_optical_frame"
+        pointStamped.header.stamp    = rospy.Time.now()
+        pointStamped.point.x = x * z
+        pointStamped.point.y = y * z
+        pointStamped.point.z = z
         
         
-        geometry_msg
-        target_point = msg.
-        x = self.camera_info.K
+        
+        #build the action goal
+        goal = pr2_controllers_msgs.msg.PointHeadGoal()     
+      #the goal consists in making the Z axis of the cameraFrame to point towards the pointStamped
+        goal.pointing_frame = "/stereo_optical_frame"
+        goal.pointing_axis.x = 0.0
+        goal.pointing_axis.y = 0.0
+        goal.pointing_axis.z = 1.0
+        goal.min_duration = rospy.Duration(0.5)
+        goal.max_velocity = 1.0
+        goal.target = pointStamped
+        
+        rospy.loginfo("Sending goal %s", str(pointStamped))
+        self.client.send_goal(goal)
+        self.client.wait_for_result(rospy.Duration(1.0))
+        
+        res = self.client.get_result()
+        if res == None:
+            rospy.logerr("Result of executing goal is none")
+            return 'failed'
+        else:
+            rospy.loginfo("Result of executing goal is %s", str(res))
+            return 'success'
+        #todo check result if res 
+        
 # main
 def main():
     
     rospy.init_node('look_at_detected_face')
-    main_sm = smach.StateMachine(outcomes=['done','failed','preempted'])
+    main_sm = smach.StateMachine(outcomes=['done', 'failed', 'preempted'])
     # Create and start the introspection server
     sis = smach_ros.IntrospectionServer('look_at_detected_face', main_sm, '/LOOK_AT_DETECTED_FACE')
-
+    
     with main_sm:
         
         smach.StateMachine.add('WAIT_FOR_FACE', GetFaceDetectionState(),
-                               transitions={'success':'LOOK_AT_FACE'},
-                                output_keys=['foo_counter_out'])
+                               transitions={'success':'LOOK_AT_FACE'})
                                    
                                    
                                      
             
-            if use_dummy_navigation_components:
-                throttle_done_transition="APPROACH_PERSON"
-            else:
-                throttle_done_transition="DISABLE_PATROL"
             
-            smach.StateMachine.add('MEET_PEOPLE_THROTTLE',ThrottleState(20*1000),
-                                   transitions={'done':throttle_done_transition,'failed':'DETECT_PEOPLE'})       
-            
-            if not use_dummy_navigation_components:
-                disable_patrol = DisablePatrolState()
-                smach.StateMachine.add('DISABLE_PATROL', disable_patrol, 
-                                       transitions = {'succeeded':'APPROACH_PERSON','aborted':'failed'})
-    
-            ## APPROACH_PERSON
-            # if use_dummy_navigation_components or no_approach_person is True:
-            # This state is created with a dummy implementation of the states that
-            # require navigation. This is to be able to test the whole state
-            # machine with the PAL simulator, and without need to execute the
-            # navigation services.
-         	no_approach_person = True
-        	use_dummy_approach = use_dummy_navigation_components or no_approach_person
-            approach_person_state = create_approach_person_state(dummy_implementation = use_dummy_approach)
-            smach.StateMachine.add('APPROACH_PERSON', approach_person_state,
-                             transitions={'done':'PRESENT_TO_PERSON'})
-            
-            
-            print options.loop
-            if options.loop:
-                last_state_transitions={'succeeded':first_state_name, 'aborted':first_state_name}
-            else:  
-                last_state_transitions={'succeeded':'done', 'aborted':'failed'}
-            
-            present_to_person = PresentToPersonState()
-            smach.StateMachine.add('PRESENT_TO_PERSON', present_to_person,
-                             transitions=last_state_transitions)
+        smach.StateMachine.add('LOOK_AT_FACE', LookToPointState(),
+                               transitions={'success':'WAIT_FOR_FACE','failed':'WAIT_FOR_FACE'})
         
-        smach.StateMachine.add('MEET_PEOPLE', core_sm,
-                               transitions={'failed':'FINAL_DISABLE_PATROL','preempted':'FINAL_DISABLE_PATROL'})
-        
-        final_disable_patrol = DisablePatrolState()
-        smach.StateMachine.add('FINAL_DISABLE_PATROL', final_disable_patrol, 
-                               transitions = {'succeeded':'failed','aborted':'failed'})
         
     sis.start()
-
-
-
-    use_action_server=True
-    if use_action_server:
-        ####################################################################################
-        # Construct action server wrapper
-        ####################################################################################
-        asw = ActionServerWrapper("meet_people_action", meet_peopleAction,
-                                  wrapped_container = main_sm,
-                                  succeeded_outcomes = [],
-                                  aborted_outcomes   = [],
-                                  preempted_outcomes = ['preempted'] )
-    
-        # Run the server
-        rospy.loginfo("Starting meet_people ActionServerWrapper ...")
-        asw.run_server() 
-        rospy.loginfo("meet_people ActionServerWrapper started")
-    	rospy.spin()
-        
-    else:
-        main_sm.execute()
+    main_sm.execute()
     
     #rospy.spin()
     sis.stop()
