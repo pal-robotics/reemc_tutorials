6e77a72e74692b213b6199cf0c887c6134369add
Luca Marchionni <luca.marchionni@pal-robotics.com>
2015-06-02 17:38:39 +0200

    Move stuff to perception_tutorials and tts_tutorials packages

--- reemc_basic_tutorials/scripts/doorStepWalkOverStart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-################################################
-#  THIS SCRIPT WILL START THE DOOR STEP ACTION #
-################################################
-
-rosservice call /walking_controller/over_obstacle "{}"
-
